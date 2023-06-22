33d1083fc3e0e0530bbe283b4318cacadfc8c757
Marco Seguri <seguri@users.noreply.github.com>
2023-06-22 10:54:16 +0200

    Initial commit

---
+++ src/main/java/name/seguri/java/elephantcarpaccioexercise/ElephantCarpaccioExerciseApplication.java
@@ -0,0 +1,13 @@
+package name.seguri.java.elephantcarpaccioexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElephantCarpaccioExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElephantCarpaccioExerciseApplication.class, args);
+	}
+
+}
